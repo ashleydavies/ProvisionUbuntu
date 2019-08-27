d31a44ee97088a6bc810b5e91cbb70446087fba4
Ashley Davies <ashley@davies.me.uk>
2019-08-27 11:42:05 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+sudo apt install fish
+curl -L https://get.oh-my.fish | fish
+omf install flash
+omf install git
+
+sudo apt install neovim python3-neovim
+sudo apt install git curl wget
+
+abbr vi nvim
+abbr vim nvim
